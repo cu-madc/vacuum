7587949d72da79ba7247402ac88dfb7352608788
Kelly Black <kjblack@gmail.com>
2011-06-22 14:08:49 -0400

    changed the ethernet setup script to create more virtual devices.

--- py/scripts/setupVirtualNICs.sh
+++ py/scripts/setupVirtualNICs.sh
@@ -7,5 +7,15 @@ sysctl -w net.ipv4.tcp_tw_recycle=1
 # Second set up the alternate ip addresses for the existing network interfaces
 ifconfig eth0:1 10.0.1.10 netmask 255.255.255.0
 ifconfig eth0:2 10.0.1.11 netmask 255.255.255.0
+ifconfig eth0:3 10.0.1.12 netmask 255.255.255.0
+ifconfig eth0:4 10.0.1.13 netmask 255.255.255.0
+ifconfig eth0:5 10.0.1.14 netmask 255.255.255.0
+ifconfig eth0:6 10.0.1.15 netmask 255.255.255.0
+ifconfig eth0:7 10.0.1.16 netmask 255.255.255.0
+ifconfig eth0:8 10.0.1.17 netmask 255.255.255.0
+ifconfig eth0:9 10.0.1.18 netmask 255.255.255.0
+ifconfig eth0:10 10.0.1.19 netmask 255.255.255.0
+ifconfig eth0:11 10.0.1.20 netmask 255.255.255.0
+ifconfig eth0:12 10.0.1.21 netmask 255.255.255.0
 
 
